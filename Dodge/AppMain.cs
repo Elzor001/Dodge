1254210b38779662c0d735aa6506c10b65035a7a
Chris Latham <BackwardSpy@gmail.com>
2014-11-06 12:16:20 +0000

    Initial Framework

---
+++ Dodge/AppMain.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Input;
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+using Sce.PlayStation.HighLevel.UI;
+
+namespace Dodge
+{
+	public class AppMain
+	{
+		private static Sce.PlayStation.HighLevel.GameEngine2D.Scene gameScene;
+
+		
+		private static SpriteUV background;
+		private static TextureInfo textureInfo;
+		private static Enemy enemy;
+		
+		public static void Main (string[] args)
+		{
+			Initialize ();
+
+			while (true) {
+				SystemEvents.CheckEvents ();
+				Update ();
+				Render ();
+				
+
+			}
+			Director.Terminate();
+		}
+
+		public static void Initialize ()
+		{
+			// Set up the graphics system
+
+			
+			Director.Initialize();
+			
+			gameScene = new Sce.PlayStation.HighLevel.GameEngine2D.Scene();
+			gameScene.Camera.SetViewFromViewport();
+			
+			textureInfo = new TextureInfo("/Application/Assets/background.png");
+			background = new SpriteUV(textureInfo);
+			background.Quad.S = textureInfo.TextureSizef;
+			gameScene.AddChild(background);
+			enemy = new Enemy(gameScene);
+
+			
+			Director.Instance.RunWithScene(gameScene, true);
+			
+		}
+
+		public static void Update ()
+		{
+			// Query gamepad for current state
+			var gamePadData = GamePad.GetData (0);
+			
+			enemy.Update();
+			
+			Director.Instance.Update();
+		}
+
+		public static void Render ()
+		{
+
+			enemy.Draw();
+			Director.Instance.Render();
+			Director.Instance.GL.Context.SwapBuffers();
+			Director.Instance.PostSwap();
+			
+		}
+	}
+}
---
+++ Dodge/Enemy.cs
@@ -0,0 +1,73 @@
+using System;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Graphics;
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+
+namespace Dodge
+{
+	public class Enemy
+	{
+		private static SpriteTile sprite;
+		private static TextureInfo textureInfo;
+		private static Texture2D texture2D;
+		private static Vector2 position;
+		private static Vector2i numTiles, tiles;
+		private static int test;
+		private static bool testC;
+		
+		
+		public Enemy (Scene scene)
+		{
+			numTiles = new Vector2i(6,3);
+			tiles = new Vector2i(0,2);
+			texture2D = new Texture2D("/Application/Assets/Flying_Fish2.png", false);
+			textureInfo = new TextureInfo(texture2D, numTiles);
+
+			sprite = new SpriteTile(textureInfo);
+			sprite.Quad.S = new Vector2(150.0f, 150.0f);
+			
+			sprite.TileIndex2D = tiles;
+			scene.AddChild(sprite);
+			position = new Vector2(100.0f, 150);
+			
+			test = 0;
+			testC = false;
+			
+		}
+		public void Update()
+		{
+			position.X-= 5.0f;
+			if(position.X < 0)
+				position.X = Director.Instance.GL.Context.GetViewport().Width;
+			sprite.Position = position;
+		}
+		public void Draw()
+		{
+			test++;
+
+			if(test > 1)
+			{
+				if(!testC)
+					tiles.X++;
+				else
+					tiles.X--;
+				if(tiles.X >= 5)
+				{
+					testC = true;
+					
+				}
+				if(tiles.X == 0)
+				{
+					testC = false;
+				}
+				test = 0;
+			}
+
+			sprite.TileIndex2D = tiles;
+		}
+	}
+}
+
