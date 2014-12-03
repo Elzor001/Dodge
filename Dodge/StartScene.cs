041ecaec251e45649fe0ab34e8577867282cce53
pscorgie <s031789c@student.staffs.ac.uk>
2014-12-03 11:48:54 +0000

    + added "Play" button to startScreen [PS]

--- Dodge/StartScene.cs
+++ Dodge/StartScene.cs
@@ -12,6 +12,11 @@ namespace Dodge
 {
 	public class StartScene : GameScene
 	{
+		
+		private SpriteUV box;
+		private TextureInfo boxtex;
+		private int screenWidth, screenHeight;
+		
 		public StartScene ()
 		{
 			scene = new Sce.PlayStation.HighLevel.GameEngine2D.Scene();
@@ -21,14 +26,47 @@ namespace Dodge
 			background = new SpriteUV(textureInfo);
 			background.Quad.S = textureInfo.TextureSizef;
 			scene.AddChild(background);
+			
+			boxtex = new TextureInfo("/Application/Assets/box2.png");
+			
+			box = new SpriteUV(boxtex);
+			box.Quad.S = new Vector2(boxtex.TextureSizef.X * 4.4f, boxtex.TextureSizef.Y*2.7f);
+			box.Position = new Vector2(205,140);
+			scene.AddChild(box);
+			
+			screenWidth = Director.Instance.GL.Context.GetViewport().Width;
+			screenHeight = Director.Instance.GL.Context.GetViewport().Height;
 		}
 		public override void Update(float dT)
 		{
 			var touches = Touch.GetData(0);
-			if(touches.Count > 0)
-				SceneManager.Instance.setInGameScene();
+			if(touches.Count > 0 && touches[0].Status == TouchStatus.Down)
+			{
+				float x = (touches[0].X +0.5f) * screenWidth;
+				float y = screenHeight -((touches[0].Y +0.5f) * screenHeight);
+				if(pressBtn1(x, y))
+					SceneManager.Instance.setInGameScene();
+			}
 			Director.Instance.Update();
 			
+		}
+		public bool pressBtn1(float x, float y)
+		{
+			
+			Bounds2 boxb = box.Quad.Bounds2();
+			float width = boxb.Point11.X;
+			float height = boxb.Point11.Y;
+			if((box.Position.X) > x)
+				return false;
+			else if(box.Position.X + width < x)
+				return false;
+			else if((box.Position.Y) > y)
+				return false;
+			else if(box.Position.Y + height < y)
+				return false;
+			else 
+				return true;
+
 		}
 		public override void Draw(float dT)
 		{
